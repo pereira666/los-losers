22a075f3308257d5632968aa5601701f48a84884
pereira666 <antcarlospereiral@gmail.com>
2021-04-09 12:38:42 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CarlosX
---
+++ src/tadas.js
@@ -0,0 +1,12 @@
+[
+{"result": "https://i.ibb.co/QChFC5b/cantadas3.jpg"},
+{"result": "https://i.ibb.co/85pbJJ8/cantadas1.jpg"},
+{"result": "https://i.ibb.co/4dLVqLg/cantadas2.jpg"},
+{"result": "https://i.ibb.co/Y3twDcn/cantadas5.jpg"},
+{"result": "https://i.ibb.co/YTDNG4X/cantadas7.jpg"},
+{"result": "https://i.ibb.co/rHRp9Df/sksjks.jpg"},
+{"result": "https://i.ibb.co/tXvZbfc/superhomfds.jpg"},
+{"result": "https://i.ibb.co/hygbYW9/Aeiru.jpg"},
+{"result": "https://i.ibb.co/T0BGLQ3/dragaobollzhd.jpg"},
+{"result": "https://i.ibb.co/5nLrYKG/ulalau.jpg"}
+]
